8dd8a30d9584c13e64e57c79af5ac2ae1865812f
vimarsh <vimarshrathod23@gmail.com>
2021-04-07 11:32:20 +0530

    Added Controller and Route for Login

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,10 +1,12 @@
+const bcrypt = require('bcryptjs');
 const db = require('../utils/db');
 
 exports.signup = (req, res, next) => {
   const userDetails = req.body;
+  const hashedPW = bcrypt.hashSync(userDetails.password, 12);
   const sql = `INSERT INTO 
   users (name, username, email, password) 
-  VALUES ('${userDetails.name}', '${userDetails.username}', '${userDetails.email}', '${userDetails.password}')`;
+  VALUES ('${userDetails.name}', '${userDetails.username}', '${userDetails.email}', '${hashedPW}')`;
 
   db.query(sql, (err, result) => {
 
@@ -21,5 +23,50 @@ exports.signup = (req, res, next) => {
       success: true,
       message: `Insertion Successul, InsertId = ${result.insertId}`
     });
+  });
+};
+
+exports.login = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const sql = `SELECT * FROM users WHERE email = '${email}'`;
+
+  db.query(sql, (err, rows, fields) => {
+
+    if(err) {
+      return res.status(500).json({
+        status: 500,
+        success: false,
+        message: err.message
+      });
+    }
+    
+    if(rows.length < 1) {
+      return res.status(401).json({
+        status: 401,
+        success: false,
+        message: 'Email is not registered'
+      }); 
+    }
+
+    const user = rows[0];
+    bcrypt.compare(password, user.password)
+      .then(doMatch => {
+        delete user.password;
+        return res.status(200).json({
+          status: 200,
+          success: true,
+          message: `Logged In Successfully!`,
+          payload: rows[0]
+        });
+      })
+      .catch(err => {
+        return res.status(401).json({
+          status: 401,
+          success: false,
+          message: `Invalid Email or Password`
+        });
+      });
+    
   });
 };
--- routes/auth.js
+++ routes/auth.js
@@ -6,4 +6,6 @@ const router = Router();
 
 router.post('/signup', authController.signup);
 
+router.post('/login', authController.login);
+
 module.exports = router;
